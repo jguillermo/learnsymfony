033d2a6bd6c413694f4937fcc9a107c957edcbe4
Jose Guillermo <jose.guillermo@orbis.com.pe>
2017-02-08 18:17:00 -0500

    feat(asd):Avvsdsdfsdfsdfsddfsdfsf

--- src/PersonBundle/Controller/DefaultController.php
+++ src/PersonBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace PersonBundle\Controller;
 
+use PersonBundle\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -12,6 +14,32 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('PersonBundle:Default:index.html.twig');
+        $person = new Person();
+        $person
+            ->setName('jose2')
+            ->setLastName('guillermo')
+            ->setSecondLastName('inche');
+
+        $personId = $person->getId();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($person);
+
+        $personId2 = $person->getId();
+
+        $em->flush();
+
+
+        $personId3 = $person->getId();
+
+        return new JsonResponse([
+            'api' => [
+                'url'     => 'hola',
+                'pid1'=>$personId,
+                'pid2'=>$personId2,
+                'pid3'=>$personId3,
+            ]
+        ]);
     }
 }
