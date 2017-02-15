0fcbfe469e684a1b8cc348d62a4c03101bd44fb5
Jose Guillermo <jguillermo@outlook.com>
2017-02-14 23:58:52 -0500

    feat(module):rutas rest

---
+++ src/Persons/Infrastructure/Ui/PersonsBundle/Controller/PersonsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Persons\Infrastructure\Ui\PersonsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class PersonsController extends Controller
+{
+
+    public function getPersonsAction()
+    {
+        return new JsonResponse([
+            'data' => []
+        ]);
+    }
+}
