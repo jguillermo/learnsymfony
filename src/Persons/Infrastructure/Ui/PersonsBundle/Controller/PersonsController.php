3604378262c93206cf1f81fb9ec10a139f605f4d
Jose Guillermo <jose.guillermo@orbis.com.pe>
2017-02-17 18:20:39 -0500

    feat(module): entidades ymsdfl

--- src/Persons/Infrastructure/Ui/PersonsBundle/Controller/PersonsController.php
+++ src/Persons/Infrastructure/Ui/PersonsBundle/Controller/PersonsController.php
@@ -4,10 +4,24 @@ namespace Persons\Infrastructure\Ui\PersonsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations\Get;
 
 class PersonsController extends Controller
 {
 
+    /**
+     * GET Route annotation.
+     * @Get("/persons/{$personId}")
+     */
+    public function getPersonAction($personId)
+    {
+        $personListSrv = $this->get("persons.person.list.srv");
+        return new JsonResponse([
+            'data' => $personListSrv->getAll([
+                'limit'=>10
+            ]),
+        ]);
+    }
     public function getPersonsAction()
     {
         $personListSrv = $this->get("persons.person.list.srv");
